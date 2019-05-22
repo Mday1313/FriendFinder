e08c19b747c67b7821a76bbca623f429f3a6b1e1
Mday1313 <melissaday076@gmail.com>
2019-05-22 13:30:48 -0400

    Modal producing proper response. Needs CSS

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,33 +4,33 @@ var surveyResults = [
     {
         name: "Inara",
         photo: "https://i.imgur.com/7UvoSRu_d.jpg?maxwidth=640&shape=thumb&fidelity=medium",
-        scores: [1,2,1,4,1,1,1,4,2,3]
+        scores: [5,4,4,5,3,4,2,4,5,1]
 
     },
     {
         name: "Bilbo",
         photo: "https://vignette.wikia.nocookie.net/lotr/images/b/b6/The_Hobbit_wallpaper_48.jpg/revision/latest?cb=20131112182330",
-        scores: [3,2,3,4,5,1,1,4,5,3]
+        scores: [2,1,5,2,1,1,5,3,3,4]
     },
     {
         name: "Ron Swanson",
         photo: "https://img.buzzfeed.com/buzzfeed-static/static/2015-09/26/13/campaign_images/webdr04/give-ron-swanson-all-of-the-bacon-2-7132-1443286870-3_dblbig.jpg",
-        scores: []
+        scores: [4,5,1,5,3,1,4,5,5,1]
     },
     {
         name: "Indiana Jones",
         photo: "https://pmcdeadline2.files.wordpress.com/2016/03/indiana-jones-5-reboot-movie.jpg?w=450&h=253&crop=1",
-        scores: []
+        scores: [3,3,5,4,2,5,1,5,3,3]
     },
     {
         name: "Sarah Connor",
         photo: "https://i.pinimg.com/originals/39/56/5e/39565ef046bc1411b34bd91a2e596ca1.gif",
-        scores: []
+        scores: [4,2,2,4,2,2,5,2,5,1]
     },
     {
         name: "Selina Meyer",
         photo: "https://amhoxie.files.wordpress.com/2014/09/big_s2.jpg",
-        scores: []
+        scores: [1,1,2,3,4,1,3,1,1,4]
     }
 ]
 
